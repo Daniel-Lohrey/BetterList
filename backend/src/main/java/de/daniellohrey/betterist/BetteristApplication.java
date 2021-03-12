c0f4336e47bef951cea11d519d65bdb6ab63b59d
Daniel Lohrey <daniellohrey@Daniels-MBP.fritz.box>
2021-03-12 12:03:55 +0100

    init backend

---
+++ backend/src/main/java/de/daniellohrey/betterist/BetteristApplication.java
@@ -0,0 +1,13 @@
+package de.daniellohrey.betterist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetteristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetteristApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/daniellohrey/betterist/BetteristApplicationTests.java
@@ -0,0 +1,13 @@
+package de.daniellohrey.betterist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetteristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
